532db8f5c764d09883bf64411036af62f039fe69
python-teacher-ren <59255101+python-teacher-ren@users.noreply.github.com>
2019-12-26 21:00:13 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+import turtle
+
+# a = input('fdshagsdajk')
+print('4444'//2)
+
+
+turtle.hideturtle()
+# turtle.color('red', 'orange')
+# turtle.shape('turtle')
+# turtle.width(4)
+# turtle.speed(6)
+#
+# turtle.begin_fill()
+# turtle.left(90)
+# turtle.forward(-100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.end_fill()
+#
+# turtle.done()
